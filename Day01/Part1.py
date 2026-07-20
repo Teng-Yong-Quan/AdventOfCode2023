b6682da5f397413da8b81122d4f32b9b0f3d9fea
Teng-Yong-Quan <tengyongquan5ki@gmail.com>
2026-07-20 17:12:19 +0800

    Convert spaces to tabs

--- Day01/Part1.py
+++ Day01/Part1.py
@@ -1,10 +1,10 @@
 input_data = open('/home/advent-of-code-2023/Day01/advent_of_code_1.txt','r')
 total = 0
 for line in input_data:
-    digits = [c for c in line if c.isdigit()]
-    if digits:
-        total += int(digits[0] + digits[-1])
-    else:
-        total += 0
+	digits = [c for c in line if c.isdigit()]
+	if digits:
+		total += int(digits[0] + digits[-1])
+	else:
+		total += 0
 print(total)
 input_data.close()

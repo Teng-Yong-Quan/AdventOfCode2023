18a253c6a29afeea72fe195039510cbc440929ed
Teng-Yong-Quan <tengyongquan5ki@gmail.com>
2026-07-20 16:59:04 +0800

    Complete Day01

---
+++ Day01/Part2.py
@@ -0,0 +1,29 @@
+input_data = open('/home/advent-of-code-2023/Day01/advent_of_code_1.txt','r')
+
+word_digit_dict = {"one" : '1', "two" : '2', "three" : '3', "four" : '4', "five" : '5', "six" : '6',
+            "seven" : '7', "eight" : '8', "nine" : '9'}
+total = 0
+for line in input_data:
+    line = line.strip()
+    start_digit = ""
+    end_digit = ""
+    word_digit = ""
+    for alphanumeric in line:
+        if alphanumeric.isdigit():
+            if start_digit == "":
+                start_digit = alphanumeric
+            end_digit = alphanumeric
+            word_digit = ""
+        else:
+            word_digit += alphanumeric
+        for word, digit in word_digit_dict.items():
+            if word in word_digit:
+                if start_digit == "":
+                    start_digit = digit
+                end_digit = digit
+                word_digit = word[1:]
+    total += int(
+        (start_digit if start_digit != "" else "0") +
+        (end_digit if end_digit != "" else "0"))
+print(total)
+input_data.close()
